771ff8054823ba634e4971d8b19598a652ebdd07
mandimartins <mandimartins23@gmail.com>
2021-07-08 15:52:01 -0300

    feat: config redux store

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
 import AppNavigator from './src/routes/routes';
 
 export default function App() {
-  return <AppNavigator />;
+
+  return <Provider store={store}>
+     <AppNavigator />
+  </Provider>;
 }
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+    tasks: () => "tasks",
+    lists: () => "lists"
+})
+
+const middlewares = [thunk]
+
+const store = createStore(rootReducer,compose(applyMiddleware(...middlewares),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+export default store;
